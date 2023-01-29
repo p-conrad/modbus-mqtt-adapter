d094fae443d3c98ff97198f0be62bc5a8930f77b
Peter Conrad <p.conrad@protonmail.ch>
2023-01-29 19:00:27 +0100

    Reduce MQTT userdata, add on_connect_fail

--- mqtt.py
+++ mqtt.py
@@ -10,25 +10,36 @@ def on_connect(client, userdata, flags, rc):
     """Callback when the MQTT server acknowledges our connect request."""
     # pylint: disable=unused-argument
     if rc == 0:
-        logging.info("Successfully connected to MQTT broker %s", userdata.target)
+        logging.info("Successfully connected to MQTT broker %s", userdata["target"])
     else:
         logging.error(
             "Connection to MQTT broker %s failed (reason: %s)",
-            userdata.target,
+            userdata["target"],
             mqtt.error_string(rc),
         )
+
+
+def on_connect_fail(client, userdata):
+    """Callback when the initial connection to the MQTT server fails."""
+    logging.error(
+        "Connection to MQTT broker %s failed",
+        userdata["target"],
+    )
+    client.on_connect_fail = None
 
 
 def on_disconnect(client, userdata, rc):
     """Callback when the connection to the MQTT server is lost."""
     # pylint: disable=unused-argument
     if rc == 0:
-        logging.info("Successfully disconnected from MQTT broker %s", userdata.target)
+        logging.info(
+            "Successfully disconnected from MQTT broker %s", userdata["target"]
+        )
     else:
         logging.error(
             "Connection to MQTT broker %s lost (reason: %s)",
-            userdata.target,
-            mqtt.error_string(rc)
+            userdata["target"],
+            mqtt.error_string(rc),
         )
 
 
@@ -40,10 +51,11 @@ def on_publish(client, userdata, mid):
 
 def create_mqtt_client(args):
     """Creates the MQTT client from all given command line arguments."""
-    client = mqtt.Client(f"MQTT-{args.device}", userdata=args)
+    client = mqtt.Client(f"MQTT-{args.device}", userdata={"target": args.target})
     if args.username:
         client.username_pw_set(args.username, args.password)
     client.on_connect = on_connect
+    client.on_connect_fail = on_connect_fail
     client.on_disconnect = on_disconnect
     client.on_publish = on_publish
     return client
